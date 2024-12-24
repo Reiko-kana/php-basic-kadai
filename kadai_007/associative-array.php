492d77ef6eeb7c00ac87378dcaa8e2e60204bdda
Reiko-kana <toza1.7nb09@gmail.com>
2024-12-24 20:27:49 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,12 @@
+<! DOCTYPE html>
+<html>
+    <head>
+        <meta charset=utf-8>
+</head>
+<body>
+    <?php
+    $vege_data = ['name' => 'onion','price' => 200,'weight' => 160];
+    print_r($vege_data);
+    ?>
+</body>
+</html>
